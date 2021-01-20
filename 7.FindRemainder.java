b088e302aa05d8fc75b6b63a673038dbbc282abd
arushi1127 <emarushi08@gmail.com>
2021-01-21 00:49:57 +0530

    Finding Remainder

---
+++ 7.FindRemainder.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    for (int i=0; i<n; i++){
+	        int a = sc.nextInt();
+	        int b = sc.nextInt();
+	        int z =0;
+	        z= a%b;
+	        System.out.println(z);
+	    }
+	}
+}
