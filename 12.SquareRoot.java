4a08d807aa33718198e4c71c1fe46a1f32ced5fb
arushi1127 <emarushi08@gmail.com>
2021-01-30 15:51:07 +0530

    Create 12.SquareRoot.java

---
+++ 12.SquareRoot.java
@@ -0,0 +1,20 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while (n-- > 0) {
+		    int num = sc.nextInt();
+		    System.out.println((int) Math.sqrt(num));
+		// your code goes here
+	}
+}
+}
