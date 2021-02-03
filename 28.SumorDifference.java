33e7fe8e7e1b028132e0e7e62a0493980986c371
arushi1127 <emarushi08@gmail.com>
2021-02-04 00:32:27 +0530

    Create 28.SumorDifference.java

---
+++ 28.SumorDifference.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if (a>b){
+		    System.out.println(a-b);
+		}
+		else 
+		    System.out.println(a+b);
+		
+		// your code goes here
+	}
+}
